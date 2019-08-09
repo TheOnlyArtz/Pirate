734810b532227f188907fc413e025c345476f436
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-09 23:24:50 +0300

    Implementing an identification process

--- R/gateway/events/identify.r
+++ R/gateway/events/identify.r
@@ -1,11 +1,18 @@
+library(rjson)
+
 # OP 10
 identify <- function(client) {
-  identiyData = data.frame(
-    token=client$token,
-    properties = data.frame(
-      "$os"=Sys.info()["sysname"], # Maybe add the build later on
-      "$browser"="Pirate v0.0.1 (Git repository: https://github.com/theonlyartz/pirate)",
-      "$device"="OnePlus 7 PRO :)"
+  identifyData = list(
+    op=2,
+    d = list(
+      token=client$token,
+      properties = list(
+        "$os"=Sys.info()["sysname"], # Maybe add the build later on
+        "$browser"="Pirate v0.0.1 (Git repository: https://github.com/theonlyartz/pirate)",
+        "$device"="OnePlus 7 PRO :)"
+      )
     )
   )
+  
+  client$ws$send(toJSON(identifyData))
 }
