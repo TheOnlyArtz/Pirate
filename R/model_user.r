fe2ffbf21097f6f9bf1d9ac336ac1289ce9966b2
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-10 10:23:25 +0300

    Added first version of user object

---
+++ R/model_user.r
@@ -0,0 +1,27 @@
+# A Class which represents a user object
+#' @export
+#'
+#' @section id The ID of the user
+#' @section username The username of the user
+#' @section discriminator The discriminator of the user (#XXXX)
+#' @section avatar The user's avatar hash
+#' @section flags The flags on a user's account
+#' @section premium_type The type of Nitro subscription on a user's account
+ClientUser <- R6Class("Class",
+  public=list(
+    initialize = function(data) {
+      id = data$id
+      username = data$username
+      discriminator = data$discriminator
+      avatar = data$discriminator
+      flags = data$flags
+      premium_type = data$premium_type
+    },
+    id = NA,
+    username = NA,
+    discriminator = NA,
+    avatar = NA,
+    flags = NA,
+    premium_type = NA
+  )
+)
