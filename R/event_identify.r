3808c2d85af5a0b6643f75e90645af4b3734a0d2
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-10 12:57:27 +0300

    Added more fields

--- R/event_identify.r
+++ R/event_identify.r
@@ -7,12 +7,18 @@ identify <- function(data, client) {
   identifyData = list(
     op=2,
     d = list(
-      token=client$token,
+      token = client$token,
       properties = list(
-        "$os"=Sys.info()["sysname"], # Maybe add the build later on
-        "$browser"="Pirate v0.0.1 (Git repository: https://github.com/theonlyartz/pirate)",
-        "$device"="OnePlus 7 PRO :)"
-      )
+        "$os" = Sys.info()["sysname"], # Maybe add the build later on
+        "$browser" = "Pirate v0.0.1 (Git repository: https://github.com/theonlyartz/pirate)",
+        "$device" = "OnePlus 7 PRO :)"
+      ),
+      compress = FALSE,
+      large_threshold = 250,
+      shard = c(0,1),
+      afk = FALSE,
+      status = "online",
+      since = Sys.time()
     )
   )
 
