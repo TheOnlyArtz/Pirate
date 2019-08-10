3319ff37d40064964b76b8cde37dd1a52db693dd
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-10 11:20:03 +0300

    Added slots description

--- R/model_guild.r
+++ R/model_guild.r
@@ -1,44 +1,44 @@
 #' A Class which represents a guild object
 #' @export
 #'
-#' @slot id
-#' @slot name
-#' @slot icon
-#' @slot splash
-#' @slot owner
-#' @slot owner_id
-#' @slot permissions
-#' @slot region
-#' @slot afk_channel_id
-#' @slot afk_timeout
-#' @slot embed_enabled
-#' @slot embed_channel_id
-#' @slot verification_level
-#' @slot default_message_notifications
-#' @slot explicit_content_filter
-#' @slot roles
-#' @slot emojis
-#' @slot features
-#' @slot mfa_level
-#' @slot application_id
-#' @slot widget_enabled
-#' @slot widget_channel_id
-#' @slot system_channel_id
-#' @slot joined_at
-#' @slot large
-#' @slot unavailable
-#' @slot member_count
-#' @slot voice_stated
-#' @slot members
-#' @slot channels
-#' @slot presences
-#' @slot max_presences
-#' @slot max_members
-#' @slot vanity_url_code
-#' @slot description
-#' @slot premium_tier
-#' @slot premium_subscription_count
-
+#' @slot id guild id
+#' @slot name guild name (2-100 characters)
+#' @slot icon	icon hash
+#' @slot splash splash hash
+#' @slot owner whether or not the user is the owner of the guild
+#' @slot owner_id id of owner
+#' @slot permissions total permissions for the user in the guild (does not include channel overrides)
+#' @slot region voice region id for the guild
+#' @slot afk_channel_id	id of afk channel
+#' @slot afk_timeout afk timeout in seconds
+#' @slot embed_enabled is this guild embeddable (e.g. widget)
+#' @slot embed_channel_id if not null, the channel id that the widget will generate an invite to
+#' @slot verification_level verification level required for the guild
+#' @slot default_message_notifications default message notifications level
+#' @slot explicit_content_filter explicit content filter level
+#' @slot roles roles in the guild
+#' @slot emojis custom guild emojis
+#' @slot features enabled guild features
+#' @slot mfa_level required MFA level for the guild
+#' @slot application_id application id of the guild creator if it is bot-created
+#' @slot widget_enabled	whether or not the server widget is enabled
+#' @slot widget_channel_id the channel id for the server widget
+#' @slot system_channel_id the id of the channel to which system messages are sent
+#' @slot joined_at when this guild was joined at
+#' @slot large whether this is considered a large guild
+#' @slot unavailable is this guild unavailable
+#' @slot member_count total number of members in this guild
+#' @slot voice_stated (without the guild_id key)
+#' @slot members users in the guild
+#' @slot channels channels in the guild
+#' @slot presences presences of the users in the guild
+#' @slot max_presences the maximum amount of presences for the guild (the default value, currently 5000, is in effect when null is returned)
+#' @slot max_members the maximum amount of members for the guild
+#' @slot vanity_url_code the vanity url code for the guild
+#' @slot description the description for the guild
+#' @slot banner banner hash
+#' @slot premium_tier premium_tier (SEE: https://discordapp.com/developers/docs/resources/guild#guild-object-premium-tier)
+#' @slot premium_subscription_count the total number of users currently boosting this server 
 Guild <- R6Class("Class",
   public = list(
     initialize <- function(data) {
