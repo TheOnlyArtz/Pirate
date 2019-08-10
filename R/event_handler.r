470056814967f88bc901cc39d878385d3823f269
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-10 10:47:58 +0300

    Use of the dispatcher

--- R/event_handler.r
+++ R/event_handler.r
@@ -1,10 +1,12 @@
 source("R/event_identify.r")
+source("R/event_dispatcher.r")
 
 handle <- function(op, data, client) {
   switch(toString(op),
     "10"={identify(data, client)},
+    "0"={dispatch(data, client)},
     {
-      print(data)
+      print(op)
     } #identify
   )
 }
