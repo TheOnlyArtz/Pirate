34233f9b2ca9efd99b75e2bb1302f77496e4fe85
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 13:45:11 +0300

    Emitting the READY event

--- R/event_ready.r
+++ R/event_ready.r
@@ -1,6 +1,5 @@
 source("R/model_user.r")
 ready <- function(data, client) {
-  client$user = User(data$user)
-  print(paste(client$user$username, toString(client$user$discriminator), sep=" "))
-  print("Connected successfully!")
+  client$user <- User(data$user)
+  client$emitter$emit("READY", client)
 }
