7ff3b451f0b5a0d1a165e4d4e2b3a9aab136a325
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 22:45:14 +0300

    Roxygen for GUILD_CREATE

--- R/event_guildcreate.r
+++ R/event_guildcreate.r
@@ -1,5 +1,8 @@
 source("R/model_guild.r")
 
+#' Event, emitted whenever the bot joins a guild
+#' @param data The new guild object
+#' @param client The client object
 guildcreate <- function(data, client) {
   new <- isFALSE(client$guilds$has(data$id))
   guild <- Guild(data)
