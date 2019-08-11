08ef7be06cd8c102d85f9f5dc15612704dbeb883
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 22:46:57 +0300

    Roxygen for READY

--- R/event_ready.r
+++ R/event_ready.r
@@ -1,4 +1,8 @@
 source("R/model_user.r")
+
+#' Event, emitted whenever the client finished the identifying phase
+#' @param data Chunks of data relevant to the client
+#' @param client The client object
 ready <- function(data, client) {
   client$private$get("session_id", data$session_id)
   client$user <- User(data$user)
