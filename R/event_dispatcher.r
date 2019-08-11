a97c81cd756bd1cf73429fa30cf424ce2c2ebbe6
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 23:28:18 +0300

    Implemented GUILD_BAN_REMOVE

--- R/event_dispatcher.r
+++ R/event_dispatcher.r
@@ -2,6 +2,8 @@ source("R/event_ready.r")
 source("R/event_guildcreate.r")
 source("R/event_guildupdate.r")
 source("R/event_guildremove.r")
+source("R/event_guildbanadd.r")
+source("R/event_guildbanremove.r")
 
 dispatch <- function(data, client) {
   client$private$set("last_sequence", data$s)
@@ -18,8 +20,13 @@ dispatch <- function(data, client) {
     "GUILD_REMOVE"={
       guildremove(data$d, client)
     },
+    "GUILD_BAN_ADD"={
+      guildbanadd(data$d, client)
+    },
+    "GUILD_BAN_REMOVE"={
+      guildbanremove(data$d, client)
+    }
     {
-      # TODO resume
     }
   )
 }
