7e2c1abe431c3b7bed31df5136f8a066bd5b76f8
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 10:01:48 +0300

    Adding guild with the new fastmap

--- R/event_guildcreate.r
+++ R/event_guildcreate.r
@@ -1,6 +1,5 @@
 source("R/model_guild.r")
 
 guildcreate <- function(data, client) {
-  client$add_guild(data$id, Guild(data))
-  print(client$guilds[[data$id]]$name)
+  client$guilds$set(data$id, Guild(data))
 }
