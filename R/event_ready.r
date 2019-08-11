9b9c45b316363ebc2fba2b5e9e953fc6d6602647
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 10:32:23 +0300

    Using S3

--- R/event_ready.r
+++ R/event_ready.r
@@ -1,6 +1,6 @@
 source("R/model_user.r")
 ready <- function(data, client) {
-  client$user = User$new(data$user)
+  client$user = User(data$user)
   print(paste(client$user$username, toString(client$user$discriminator), sep=" "))
   print("Connected successfully!")
 }
