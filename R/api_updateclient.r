e8779b79027e9f0eea533a3fdf7c467eddd50e11
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-11 22:12:16 +0300

    Added notrun bound to the examples

--- R/api_updateclient.r
+++ R/api_updateclient.r
@@ -11,8 +11,8 @@
 #' Sending malformed payload will result in errors! don't panick :)
 #'
 #' @examples
-#' update.presence(client, list(game=list(name="Hey!", type=0)))
-#' update.presence(client, list(game=list(name="Streaming!", type=1, url="https://twitch.tv/theonlyartz")))
+#' \dontrun{update.presence(client, list(game=list(name="Hey!", type=0)))}
+#' \dontrun{update.presence(client, list(game=list(name="Streaming!", type=1, url="https://twitch.tv/theonlyartz")))}
 update.presence <- function(client, presence) {
   payload <- list(
     op = 3,
