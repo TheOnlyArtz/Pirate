1412022f6be31dd8a706de7c4c9fba626fb455a8
callcraft456@gmail.com <callcraft456@gmail.com>
2019-08-13 15:39:17 +0300

    Moved docs position

--- R/resolvers.r
+++ R/resolvers.r
@@ -1,9 +1,8 @@
 options(digits = 22)
+DISCORD_EPOCH <- 1420070400000
 #' A function which resolves an ID to a timestamp in ms
 #' @export
 #' @param id The ID you want to resolve
-DISCORD_EPOCH <- 1420070400000
-
 resolve.snowflake <- function(id) {
-  print(floor((as.numeric(id)/(2^22)) + DISCORD_EPOCH))
+  floor((as.numeric(id)/(2^22)) + DISCORD_EPOCH)
 }
